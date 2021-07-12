1ca149582428893c6fe1da7d3107ce371c81a4d4
Francisco Farias <kiofarias@gmail.com>
2021-07-12 17:20:25 -0300

    Initializing kfs-pet-clinic project

---
+++ src/main/java/kfs/springframework/kfspetclinic/KfsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package kfs.springframework.kfspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KfsPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kfs/springframework/kfspetclinic/KfsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package kfs.springframework.kfspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfsPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

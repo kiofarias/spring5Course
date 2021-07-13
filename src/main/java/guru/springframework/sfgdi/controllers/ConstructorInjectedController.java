b1761dfc06a3a60dc97381edc56889a663fbfccd
Francisco Farias <kiofarias@gmail.com>
2021-07-13 09:44:02 -0300

    Lesson 35. Dependency Injection using Spring-- Example of how to Spring works with Contructors, setters and properties dependency injection

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,12 +1,15 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
 
-
+    @Autowired /*@Autowired is optional when using Dependency injection with class constructors*/
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }

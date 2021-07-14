fedd30a58f6d29f863ef76b3bdeb22a1741d7746
Francisco Farias <kiofarias@gmail.com>
2021-07-14 17:18:56 -0300

    Impl Map based Services. closes #5.

---
+++ pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package kfs.springframework.kfspetclinic.services.map;
+
+import kfs.springframework.kfspetclinic.model.Pet;
+import kfs.springframework.kfspetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+}

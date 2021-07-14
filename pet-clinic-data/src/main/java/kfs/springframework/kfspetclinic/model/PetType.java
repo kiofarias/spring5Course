c27bb868fc5742edda5816d2ea0834a46a6a9742
Francisco Farias <kiofarias@gmail.com>
2021-07-14 13:49:16 -0300

    Updated PetType.

--- pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/model/PetType.java
@@ -1,8 +1,10 @@
 package kfs.springframework.kfspetclinic.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class PetType {
     private String name;
 }

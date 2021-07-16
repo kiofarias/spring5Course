56bad9bf87e8baaff2f111b898a8df7b586d39c3
Francisco Farias <kiofarias@gmail.com>
2021-07-16 12:54:41 -0300

    Lesson 72. Component Scan-- Refactoring we should see how Spring scan packages to create beans and how to configure this scan with annotation @ComponentScan.

--- src/main/java/guru/springframework/sfgdi/services/LifeCycleDemoBean.java
+++ src/main/java/guru/springframework/sfgdi/services/LifeCycleDemoBean.java
@@ -18,15 +18,9 @@ public class LifeCycleDemoBean implements InitializingBean, DisposableBean, Bean
     }
 
     @Override
-    public void destroy() throws Exception {
-        System.out.println("## The Lifecycle bean has been terminated");
+    public void setBeanName(String name) {
+        System.out.println("## My Bean Name is: " + name);
     }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("## The LifeCycleBean has its properties set!");
-    }
-
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
@@ -34,13 +28,12 @@ public class LifeCycleDemoBean implements InitializingBean, DisposableBean, Bean
     }
 
     @Override
-    public void setBeanName(String name) {
-        System.out.println("## My Bean Name is: " + name);
-    }
-
-    @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("## Application context has been set");
+    }
+
+    public void beforeInit(){
+        System.out.println("## - Before Init - Called by Bean Post Processor");
     }
 
     @PostConstruct
@@ -48,18 +41,25 @@ public class LifeCycleDemoBean implements InitializingBean, DisposableBean, Bean
         System.out.println("## The Post Construct annotated method has been called");
     }
 
-    @PreDestroy
-    public void preDestroy() {
-        System.out.println("## The Predestroy annotated method has been called");
-    }
-
-    public void beforeInit(){
-        System.out.println("## - Before Init - Called by Bean Post Processor");
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("## The LifeCycleBean has its properties set!");
     }
 
     public void afterInit(){
         System.out.println("## - After init called by Bean Post Processor");
     }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("## The Predestroy annotated method has been called");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("## The Lifecycle bean has been terminated");
+    }
+
 
 }
 

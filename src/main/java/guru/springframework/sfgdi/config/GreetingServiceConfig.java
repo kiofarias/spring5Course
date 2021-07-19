8f1e6d5148e8cbb805831aab7dbef0b61ab999b6
Francisco Farias <kiofarias@gmail.com>
2021-07-19 13:52:05 -0300

    Lesson 74. Primary Beans and Profiles

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,33 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyGreetingService;
-import guru.springframework.sfgdi.services.SetterGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"EN", "default"})
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile("PT-BR")
+    @Bean
+    I18nPortugueseGreetingService i18nService(){
+        return new I18nPortugueseGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
     @Bean
     ConstructorGreetingService constructorGreetingService(){
         return new ConstructorGreetingService();
--- src/main/java/guru/springframework/sfgdi/services/I18nPortugueseGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nPortugueseGreetingService.java
@@ -1,10 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Profile("PT-BR")
-@Service("i18nService")
 public class I18nPortugueseGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {

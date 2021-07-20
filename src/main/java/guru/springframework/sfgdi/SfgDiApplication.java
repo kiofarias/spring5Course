fa1bfe71ada283630459da8589d44c55c4c1c00f
Francisco Farias <kiofarias@gmail.com>
2021-07-20 09:38:55 -0300

    Lesson 79.Spring Bean Scope Example

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.services.PrototypeBean;
+import guru.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -48,6 +51,18 @@ public class SfgDiApplication {
         ConstructorInjectedController constructorInjectedController =(ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 
         System.out.println(constructorInjectedController.getGreeting());
+
+        System.out.println("----------Bean Scopes----------");
+        SingletonBean singletonBean = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean.getMyScope());
+        SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean2.getMyScope());
+
+        PrototypeBean prototypeBean = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean.getMyScope());
+        PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean2.getMyScope());
+
     }
 
 }

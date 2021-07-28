170d2b7197151b7724af8798b6885ed3e1d09b5f
Francisco Farias <kiofarias@gmail.com>
2021-07-28 14:50:49 -0300

    Added Speciality Service Map impl. closes #31

---
+++ pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package kfs.springframework.kfspetclinic.services;
+
+import kfs.springframework.kfspetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package kfs.springframework.kfspetclinic.services.map;
+
+import kfs.springframework.kfspetclinic.model.Speciality;
+import kfs.springframework.kfspetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality,Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}

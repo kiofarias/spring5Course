8afb79ec87dd76bc66deb388767c5d3beffa8c2c
Francisco Farias <kiofarias@gmail.com>
2021-07-30 16:55:16 -0300

    Lesson 138. Many To Many JPA Relationships

--- src/main/java/domain/Recipe.java
+++ src/main/java/domain/Recipe.java
@@ -34,6 +34,12 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -130,4 +136,11 @@ public class Recipe {
         this.notes = notes;
     }
 
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }

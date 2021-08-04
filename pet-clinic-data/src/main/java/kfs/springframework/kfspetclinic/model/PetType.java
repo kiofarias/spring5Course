4998d64b75f2b499a6593bd9251b873ab6b81c82
Francisco Farias <kiofarias@gmail.com>
2021-08-04 09:49:34 -0300

    Updated Owner, Pet and PetType with JPA annotations. closes #36

--- pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/model/PetType.java
@@ -3,8 +3,15 @@ package kfs.springframework.kfspetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 }

3850fa624a51951fb9fc9ce38e7779ea3e84160a
Francisco Farias <kiofarias@gmail.com>
2021-08-04 10:25:40 -0300

    Updated Vet and Speciality with JPA annotations. closes #37

--- pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/kfs/springframework/kfspetclinic/model/Speciality.java
@@ -3,9 +3,17 @@ package kfs.springframework.kfspetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 }

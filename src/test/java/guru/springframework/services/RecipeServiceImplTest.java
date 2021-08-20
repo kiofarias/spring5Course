ad92461cf512db7c3c2e0379f775ee36af5fcd62
Francisco Farias <kiofarias@gmail.com>
2021-08-20 13:51:42 -0300

    194. Display a Recipe by ID

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -23,13 +24,27 @@ public class RecipeServiceImplTest {
     @Before
     public void setUp(){
         MockitoAnnotations.initMocks(this);
-
         recipeService = new RecipeServiceImpl(recipeRepository);
 
     }
 
     @Test
-    public void getRecipes() {
+    public void getRecipeByIdTest(){
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> optionalRecipe = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(optionalRecipe);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned",recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();;
+    }
+
+    @Test
+    public void getRecipesTest() {
 
         Recipe recipe = new Recipe();
         Set<Recipe> recipesData= new HashSet<>();
@@ -42,4 +57,6 @@ public class RecipeServiceImplTest {
         assertEquals(recipes.size(),1);
         verify(recipeRepository, times(1)).findAll();
     }
+
+
 }

af6992ba04b144acb2eb66884987ea7e388262a6
Francisco Farias <kiofarias@gmail.com>
2021-09-20 13:15:26 -0300

    212. Displaying images from Database

--- src/main/java/guru/springframework/controllers/ImageController.java
+++ src/main/java/guru/springframework/controllers/ImageController.java
@@ -1,12 +1,18 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.ImageService;
 import guru.springframework.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -30,5 +36,26 @@ public class ImageController {
                                   @RequestParam("imagefile") MultipartFile multipartFile){
         imageService.saveImageFile(Long.valueOf(recipeId), multipartFile);
         return "redirect:/recipe/"+recipeId+"/show";
+    }
+
+    @GetMapping("/recipe/{recipeId}/recipeimage")
+    public void renderImageFromDB(@PathVariable String recipeId
+            , HttpServletResponse httpServletResponse) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+
+        if (recipeCommand.getImage() != null){
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+
+            int i=0;
+
+            for(Byte wrappedByte: recipeCommand.getImage()){
+                byteArray[i++]= wrappedByte;
+            }
+
+            httpServletResponse.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, httpServletResponse.getOutputStream());
+        }
+
     }
 }
--- src/test/java/guru/springframework/controllers/ImageControllerTest.java
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -7,10 +7,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -57,6 +61,35 @@ public class ImageControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(header().string("Location", "/recipe/1/show"));
         verify(imageService, times(1)).saveImageFile(anyLong(),any());
+
+    }
+
+    @Test
+    public void renderImageFromDB() throws Exception{
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        String s = "fake image test";
+        Byte[] bytesBoxed = new Byte[s.getBytes().length];
+
+        int i=0;
+        for(byte primByte :s.getBytes()){
+            bytesBoxed[i++]=primByte;
+        }
+
+        recipeCommand.setImage(bytesBoxed);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+        //when
+        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+        byte[] responseBytes = response.getContentAsByteArray();
+        //then
+
+        assertEquals(s.getBytes().length,responseBytes.length);
 
     }
 

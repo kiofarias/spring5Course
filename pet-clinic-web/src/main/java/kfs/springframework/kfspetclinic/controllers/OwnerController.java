7e89da1f2ad1b96aed4c720c020c14399d342c41
Francisco Farias <kiofarias@gmail.com>
2021-11-27 14:45:23 -0300

    Added create and update owner. Closes #55

--- pet-clinic-web/src/main/java/kfs/springframework/kfspetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kfs/springframework/kfspetclinic/controllers/OwnerController.java
@@ -6,41 +6,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM="owners/createOrUpdateOwnerForm";
     private final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
 
-//    @RequestMapping({"","/","/index","/index.html"})
-//    public String listOwners(Model model){
-//        model.addAttribute("owners", ownerService.findAll());
-//        return "owners/index";
-//    }
-
     @InitBinder
     public void setAllowedFields(WebDataBinder webDataBinder){
         webDataBinder.setDisallowedFields("id");
     }
 
-    @RequestMapping("/find")
+    @GetMapping("/find")
     public String findOwners(Model model){
         model.addAttribute("owner", Owner.builder().build());
         return "owners/findOwners";
     }
 
-    @RequestMapping
+    @GetMapping
     public String processFindForm(Owner owner, BindingResult result ,Model model){
         if(owner.getLastName()==null){
             owner.setLastName("");
@@ -58,11 +52,42 @@ public class OwnerController {
         return "owners/ownersList";
     }
 
-    @RequestMapping("/{ownerId}")
-    public ModelAndView listOwnerDetails(@PathVariable Long ownerId){
+    @GetMapping("/{ownerId}")
+    public ModelAndView listOwnerDetails(@PathVariable("ownerId") Long ownerId){
         ModelAndView mav= new ModelAndView("owners/ownerDetails");
         mav.addObject(ownerService.findById(ownerId));
         return mav;
+    }
+
+    @GetMapping("/new")
+    public String initCreationForm(Model model){
+        model.addAttribute("owner",Owner.builder().build());
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Valid Owner owner, BindingResult result){
+        if(result.hasErrors()){
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        }
+        Owner ownerSaved = ownerService.save(owner);
+        return  "redirect:/owners/"+ownerSaved.getId();
+    }
+
+    @GetMapping("/{ownerId}/edit")
+    public String initUpdateForm(@PathVariable("ownerId") Long ownerId, Model model){
+        model.addAttribute("owner",ownerService.findById(ownerId));
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/{ownerId}/edit")
+    public String processUpdateForm(@Valid Owner owner, @PathVariable("ownerId") Long ownerId, BindingResult result){
+        if(result.hasErrors()){
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        }
+        owner.setId(ownerId);
+        Owner ownerSaved = ownerService.save(owner);
+        return  "redirect:/owners/"+ownerSaved.getId();
     }
 }
 
--- pet-clinic-web/src/test/java/kfs/springframework/kfspetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/kfs/springframework/kfspetclinic/controllers/OwnerControllerTest.java
@@ -16,9 +16,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -82,5 +82,43 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/ownerDetails"))
                 .andExpect(model().attribute("owner",hasProperty("id",is(1L))));
+    }
+
+    @Test
+    void initCreationForm() throws Exception{
+        mockMvc.perform(get("/owners/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"))
+                .andExpect(model().attributeExists("owner"));
+        verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void processCreationForm() throws Exception{
+        when(ownerService.save(any())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(post("/owners/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"))
+                .andExpect(model().attributeExists("owner"));
+        verify(ownerService, times(1)).save(any());
+    }
+
+    @Test
+    void initUpdateForm() throws Exception{
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(get("/owners/1/edit"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"))
+                .andExpect(model().attributeExists("owner"));
+    }
+
+    @Test
+    void processUpdateForm() throws Exception{
+        when(ownerService.save(any())).thenReturn(Owner.builder().id(1L).build());
+        mockMvc.perform(post("/owners/1/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"))
+                .andExpect(model().attributeExists("owner"));
+        verify(ownerService, times(1)).save(any());
     }
 }

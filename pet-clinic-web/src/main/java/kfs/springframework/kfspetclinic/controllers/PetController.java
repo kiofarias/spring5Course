59df4d6fe28b06dc686ec9f0e548b0922ba059a9
Francisco Farias <kiofarias@gmail.com>
2021-11-27 17:04:45 -0300

    added add pet and update pet.closes #56

--- pet-clinic-web/src/main/java/kfs/springframework/kfspetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/kfs/springframework/kfspetclinic/controllers/PetController.java
@@ -1,24 +1,26 @@
 package kfs.springframework.kfspetclinic.controllers;
 
 import kfs.springframework.kfspetclinic.model.Owner;
+import kfs.springframework.kfspetclinic.model.Pet;
 import kfs.springframework.kfspetclinic.model.PetType;
 import kfs.springframework.kfspetclinic.services.OwnerService;
 import kfs.springframework.kfspetclinic.services.PetService;
 import kfs.springframework.kfspetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 @Controller
 @RequestMapping("/owners/{ownerId}")
 public class PetController {
 
-    private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM="owners/createOrUpdateOwnerForm";
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM="pets/createOrUpdatePetForm";
     private final OwnerService ownerService;
     private final PetService petService;
     private final PetTypeService petTypeService;
@@ -43,5 +45,47 @@ public class PetController {
     public void setAllowedFields(WebDataBinder webDataBinder){
         webDataBinder.setDisallowedFields("id");
     }
+
+    @GetMapping("/pets/new")
+    public String initCreationForm(Owner owner, Model model){
+        Pet pet = new Pet();
+        owner.getPets().add(pet);
+        model.addAttribute("pet",pet);
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/new")
+    public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult bindingResult, Model model){
+        if(StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(),true)!= null){
+            bindingResult.rejectValue("name","duplicate", "already exists");
+        }
+        owner.getPets().add(pet);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("pet",pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        }
+        petService.save(pet);
+        return "redirect:/owners/"+owner.getId();
+    }
+
+    @GetMapping("/pets/{petId}/edit")
+    public String initUpdateForm(@PathVariable Long petId, Model model){
+        model.addAttribute("pet",petService.findById(petId));
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/{petId}/edit")
+    public String processUpdateForm(Owner owner, @Valid Pet pet, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            pet.setOwner(owner);
+            model.addAttribute("pet",pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        }
+        owner.getPets().add(pet);
+        petService.save(pet);
+        return "redirect:/owners/"+owner.getId();
+    }
+
+
 
 }
---
+++ pet-clinic-web/src/test/java/kfs/springframework/kfspetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,108 @@
+package kfs.springframework.kfspetclinic.controllers;
+
+import kfs.springframework.kfspetclinic.model.Owner;
+import kfs.springframework.kfspetclinic.model.Pet;
+import kfs.springframework.kfspetclinic.model.PetType;
+import kfs.springframework.kfspetclinic.services.OwnerService;
+import kfs.springframework.kfspetclinic.services.PetService;
+import kfs.springframework.kfspetclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@ExtendWith(MockitoExtension.class)
+class PetControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @Mock
+    PetService petService;
+
+    @InjectMocks
+    PetController petController;
+
+    Set<PetType> petTypes;
+
+    Owner owner;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder().id(1L).build();
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1L).name("Dog").build());
+        petTypes.add(PetType.builder().id(2L).name("Cat").build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(petController)
+                .build();
+    }
+
+    @Test
+    void initCreationForm() throws Exception{
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+        mockMvc.perform(get("/owners/1/pets/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+        verifyNoInteractions(petService);
+    }
+
+    @Test
+    void processCreationForm() throws Exception{
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+        mockMvc.perform(post("/owners/1/pets/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+        verify(petService,times(1)).save(any());
+    }
+
+    @Test
+    void initUpdateForm() throws Exception{
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().id(2L).build());
+        mockMvc.perform(get("/owners/1/pets/2/edit"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+
+    }
+
+    @Test
+    void processUpdateForm() throws Exception{
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+        mockMvc.perform(post("/owners/1/pets/2/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+        verify(petService, times(1)).save(any());
+    }
+}

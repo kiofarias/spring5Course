92c9f4e8177df4ad1326a6a68384b8e201c68b86
Francisco Farias <kiofarias@gmail.com>
2022-01-11 13:31:12 -0300

    added json endpoint to vets. closes #66

--- pet-clinic-web/src/main/java/kfs/springframework/kfspetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/kfs/springframework/kfspetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package kfs.springframework.kfspetclinic.controllers;
 
+import kfs.springframework.kfspetclinic.model.Vet;
 import kfs.springframework.kfspetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,5 +24,11 @@ public class VetController {
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    @ResponseBody
+    public  Set<Vet> getListVetsJson(){
+        return vetService.findAll();
     }
 }

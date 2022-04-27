c89e5c2393c38c479a319c232bad2f7a3fe6bcdc
Francisco Farias <kiofarias@gmail.com>
2022-04-26 23:00:56 -0300

    419. Assignment Review: Implement POST for Vendors

--- src/main/java/br/com/springframework/spring5webfluxrest/controller/VendorController.java
+++ src/main/java/br/com/springframework/spring5webfluxrest/controller/VendorController.java
@@ -2,6 +2,8 @@ package br.com.springframework.spring5webfluxrest.controller;
 
 import br.com.springframework.spring5webfluxrest.domain.Vendor;
 import br.com.springframework.spring5webfluxrest.repository.VendorRepository;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,5 +26,11 @@ public class VendorController {
     @GetMapping("/{id}")
     Mono<Vendor> getVendorById(@PathVariable String id){
         return vendorRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    Flux<Vendor> createVendor(@RequestBody Publisher<Vendor> vendorStream){
+        return vendorRepository.saveAll(vendorStream);
     }
 }
--- src/test/java/br/com/springframework/spring5webfluxrest/controller/VendorControllerTest.java
+++ src/test/java/br/com/springframework/spring5webfluxrest/controller/VendorControllerTest.java
@@ -4,13 +4,18 @@ import br.com.springframework.spring5webfluxrest.domain.Vendor;
 import br.com.springframework.spring5webfluxrest.repository.VendorRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 public class VendorControllerTest {
 
@@ -48,5 +53,26 @@ public class VendorControllerTest {
                 .exchange()
                 .expectBodyList(Vendor.class)
                 .hasSize(1);
+    }
+
+    @Test
+    public void createVendor(){
+        Flux<Vendor> vendorFluxSaved = Flux.just(Vendor.builder().firstName("saved1").build()
+                ,Vendor.builder().firstName("saved2").build());
+        given(vendorRepository.saveAll(any(Flux.class))).willReturn(vendorFluxSaved);
+
+        Flux<Vendor> vendorFlux =  Flux.just(Vendor.builder().firstName("send1").build(),
+                Vendor.builder().firstName("send2").build());
+
+        webTestClient.post().uri("/api/v1/vendors")
+                .body(vendorFlux,Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBodyList(Vendor.class)
+                .isEqualTo(vendorFluxSaved.collectList().block())
+                .hasSize(2);
+
+        then(vendorRepository).should(times(1)).saveAll(any(Publisher.class));
     }
 }
